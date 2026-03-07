30bc508eb7f8d84f11ab4a088db098df767653eb
Oscar Saraza <oscarsaraza@gmail.com>
2026-03-07 08:21:31 -0500

    Página /login de proyecto web

---
+++ web/src/lib/components/text-button.svelte
@@ -0,0 +1,5 @@
+<button
+  class="text-primary-950 text-sm font-medium hover:text-primary-500 transition-colors"
+>
+  <slot />
+</button>
---
+++ web/src/routes/login/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Icon from "$lib/components/icons/settings.svelte";
+  import QrCode from "$lib/components/qr-code.svelte";
+  import TextButton from "$lib/components/text-button.svelte";
+</script>
+
+<main class="min-h-screen flex flex-col justify-center">
+  <div class="flex items-center justify-center w-full mx-auto">
+    <div
+      class="bg-secondary-800 w-lg rounded-3xl p-12 flex flex-col gap-8 items-center"
+    >
+      <h1>
+        ¡Hola! <span class="text-primary-500">Escanea</span> para entrar
+      </h1>
+
+      <div class="flex items-center justify-center gap-2 w-full">
+        <Icon fill="fill-primary-500"></Icon>
+
+        <span class="text-sm tracking-wide text-[#B3ACA0]"
+          >Abre la aplicación en tu dispositivo móvil y apunta a la pantalla</span
+        >
+      </div>
+
+      <a href="/">
+        <div class="w-64 h-64">
+          <QrCode />
+        </div>
+      </a>
+
+      <TextButton>Refrescar código</TextButton>
+    </div>
+  </div>
+</main>
